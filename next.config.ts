347e6f53a654c70598cb2b191207b599c49ca9fb
Abdul Mutalib <mutalib92847@gmail.com>
2026-08-02 18:42:03 +0530

    fix(next.config.ts): Solve the development cross-origin problem

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,9 @@
-import type { NextConfig } from "next";
-import "./lib/env.mjs";
+import type { NextConfig } from 'next';
+import './lib/env.mjs';
 
 const nextConfig: NextConfig = {
   /* config options here */
+  allowedDevOrigins: ['127.0.0.1'],
 };
 
 export default nextConfig;
